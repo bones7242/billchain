2523ef649ba3d69ac78df18416f773f3e312ee28
bill bittner <bittner.w@gmail.com>
2018-03-28 11:23:59 -0700

    added merkleroot function

---
+++ utils/merkleRoot.js
@@ -0,0 +1,27 @@
+function getMerkleRoot (transactionsArray) {
+    const hashSeed = 'billbitt';
+    let count = transactionsArray.length;
+    let previousTreeLayer = [];
+    transactionsArray.forEach(transaction => {
+        previousTreeLayer.push(transaction.txid);
+    })
+    let treeLayer = previousTreeLayer;
+    while (count > 1) {
+        treeLayer = [];
+        for (let i = 1; i < previousTreeLayer.length; i++){
+            const hash = crypto.createHmac('sha256', hashSeed)
+                .update(previousTreeLayer[i-1] + previousTreeLayer[i])
+                .digest('hex');
+            treeLayer.push(hash);
+        }
+        count = treeLayer.length;
+        previousTreeLayer = treeLayer;
+    }
+    let merkleRoot = '';
+    if (treeLayer.length === 1) {
+       merkleRoot = treeLayer[0];
+    };
+    return merkleRoot;
+}
+
+module.exports = getMerkleRoot;
